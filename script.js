e1e8b092c7d639741b52fb38905d47dd1f4d835e
Vitraag Shete <vitraagshete25@gmail.com>
2024-05-17 12:13:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function matchpassword(){
+var firstPass=document.f1.password.value;
+var secondPass=document.f1.password2.value;
+if(firstPass==secondPass)
+    {
+        alert("Your Password is correct👍👍👍👍");
+        return true;
+}
+else{
+    alert("Password must be same🤔🤔🤔🤔");
+    return false;
+}
+}
